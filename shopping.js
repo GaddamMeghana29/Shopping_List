dffd46590bc69e28047fee1d26deb89235af9e9d
GaddamMeghana29 <127496425+GaddamMeghana29@users.noreply.github.com>
2023-04-25 17:04:23 +0530

    Used code formatter

--- shopping.js
+++ shopping.js
@@ -1,58 +1,35 @@
-let input     =  document.getElementById('textinput');
-let listContainer =  document.getElementById('list-container');
+let input = document.getElementById('textinput');
+let listContainer = document.getElementById('list-container');
 
 input.addEventListener('keydown', function(event) {
-  if (event.key == 'Enter') {
-    addItem();
-  }
+	if (event.key == 'Enter') {
+		addItem();
+	}
 });
 
-function addItem(){
-  let newitem={
-    itemName:input.value,
-    ismarked:false
-  }
-   if (newitem.itemName.trim() != ''){
-    let li = document.createElement("li");
-    li.innerHTML = newitem.itemName;
-    li.classList.add("liststyle");
-    listContainer.appendChild(li);
-    let span =document.createElement("span");
-    span.innerHTML = "\u00d7";
-    li.appendChild(span);
-    }
-    input.value='';
+function addItem() {
+	let newitem = {
+		itemName: input.value,
+		ismarked: false
+	}
+	if (newitem.itemName.trim() != '') {
+		let li = document.createElement("li");
+		li.innerHTML = newitem.itemName;
+		li.classList.add("liststyle");
+		listContainer.appendChild(li);
+		let span = document.createElement("span");
+		span.innerHTML = "\u00d7";
+		li.appendChild(span);
+	}
+	input.value = '';
 }
 
 
 
-listContainer.addEventListener("click",function(e){
-  if(e.target.tagName === "LI"){
-     e.target.classList.toggle("checked");
-  }
- else if(e.target.tagName === "SPAN"){
-    e.target.parentElement.remove();
-  }
-}, false); 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+listContainer.addEventListener("click", function(e) {
+	if (e.target.tagName === "LI") {
+		e.target.classList.toggle("checked");
+	} else if (e.target.tagName === "SPAN") {
+		e.target.parentElement.remove();
+	}
+}, false);
